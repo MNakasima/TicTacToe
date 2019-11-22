e6e5fc6f20cba7287f029d3094e7c28e36d79be4
Maruko <marco.k95@hotmail.com>
2019-11-22 16:04:07 -0300

    login page design

---
+++ app/src/main/java/com/mnakasima/tictactoe/Login.kt
@@ -0,0 +1,17 @@
+package com.mnakasima.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun buLoginEvent(view: View){
+
+    }
+}
